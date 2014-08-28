7e547bf84c9a81174d2d8c3847a88a658e576ac0
Ron Meng <ron.meng@datadoghq.com>
2014-08-28 14:00:45 +0000

    Revert "use makeStringSorter"
    This reverts commit 12c6467f72e6349296033a99d99f178059721d47.

--- tablemutt.js
+++ tablemutt.js
@@ -222,20 +222,23 @@
     };
 
     TableMutt.prototype._makeSorter = function (direction, transform) {
-        var lowerCaseTransform = function (x) { return x.toLocaleLowerCase(); }
-        var compVal = (direction === ASCENDING) ? 1 : -1;
-        return function (a, b) {
-            var aval = transform(a);
-            var bval = transform(b);
-
-            if (aval.constructor === String) {
-                return (TableMutt.prototype._makeStringSorter(direction, lowerCaseTransform))(aval,bval);
-            }
-
-            if (aval > bval) { return compVal; }
-            else if (aval < bval) { return -1*compVal; }
-            else { return 0; }
-        };
+        if (direction === ASCENDING) {
+            return function (a, b) {
+                var aval = transform(a);
+                var bval = transform(b);
+                if (aval > bval) { return 1; }
+                else if (aval < bval) { return -1; }
+                else { return 0; }
+            };
+        } else if (direction === DESCENDING) {
+            return function (a, b) {
+                var aval = transform(a);
+                var bval = transform(b);
+                if (aval > bval) { return -1; }
+                else if (aval < bval) { return 1; }
+                else { return 0; }
+            };
+        }
     };
 
     TableMutt.prototype._makeNoopTransform = function (id) {
@@ -314,11 +317,12 @@
                         return self.columns[columnId].compare(b, a);
                     }
                 };
+                self._sorters.push(sorter);
             } else {
                 // Default to comparing transformed vals
                 sorter = self._makeSorter(direction, transform);
+                self._sorters.push(sorter);
             }
-            self._sorters.push(sorter);
         });
     };
 
